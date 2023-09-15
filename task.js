1b4755579cd1010f7e40c3386cadfc4909c6c583
povilius <povilius@yahoo.com>
2023-09-15 20:39:57 +0300

    Finished tasks

--- task.js
+++ task.js
@@ -17,8 +17,30 @@ if (!password.includes(`#`)) {
 
 // Antra užduotis
 
-let personsAge = 5
+let personsAge = prompt(`Įveskite savo amžių:`)
 
-if (personsAge <= 5) {
+if (personsAge < 0) {
+  console.log(`Įvestas amžius yra per mažas`)
+} else if (personsAge > 110) {
+  console.log(`Įvestas amžius yra per didelis`)
+} else if (personsAge < 6) {
   console.log(`Į mokyklą neina`)
+} else if (personsAge >= 7 && personsAge <= 10) {
+  console.log(`Eina į pradinę klasę`)
+} else if (personsAge >= 11 && personsAge <= 14) {
+  console.log(`Eina į pagrindinę klasę`)
+} else if (personsAge >= 15 && personsAge <= 18) {
+  console.log(`Eina į gimnaziją`)
+} else {
+  console.log(`Mokyklą baigė`)
+}
+
+if (personsAge === 6) {
+  console.log(`Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas.`)
+} else if (personsAge === 10) {
+  console.log(`Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.`)
+} else if (personsAge === 14) {
+  console.log(`Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas.`)
+} else if (personsAge === 18) {
+  console.log(`Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs.`)
 }
